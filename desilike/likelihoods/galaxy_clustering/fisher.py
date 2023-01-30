fixing bug with some of precision matrices

desilike/likelihoods/galaxy_clustering/fisher.py
@@ -37,7 +37,7 @@ class SNWeightedPowerSpectrumLikelihood(BaseGaussianLikelihood):
             footprints = [footprints] * len(self.theories)
         self.footprints = footprints
         if klim is not None:
-            k = np.linspace(*klim, num=100)
+            k = np.linspace(*klim, num=500)  # above, jax takes *a lot* of memory in reverse mode
             for theory in self.theories: theory.init.update(k=k)
         self.theories = EnsembleCalculator(calculators=theories)
         if np.ndim(mu) == 0:

desilike/tests/test_differentiation.py
@@ -73,7 +73,7 @@ def test_fisher_galaxy():
     theory = KaiserTracerPowerSpectrumMultipoles(template=DirectPowerSpectrumTemplate(z=0.5))
     #for param in theory.params.select(basename=['alpha*', 'sn*']): param.update(derived='.best')
     observable = TracerPowerSpectrumMultipolesObservable(klim={0: [0.05, 0.2, 0.01], 2: [0.05, 0.18, 0.01]},
-                                                         data='_pk/data.npy', mocks='_pk/mock_*.npy', wmatrix='_pk/window.npy',
+                                                         data='_pk/data.npy', covariance='_pk/mock_*.npy', wmatrix='_pk/window.npy',
                                                          theory=theory)
     likelihood = ObservablesGaussianLikelihood(observables=[observable], scale_covariance=False)
     likelihood.all_params['logA'].update(derived='jnp.log(10 *  {A_s})', prior=None)

desilike/tests/test_parameter.py
@@ -37,7 +37,9 @@ def test_matrix():
     params = {name: {'prior': {'dist': 'norm', 'loc': 1., 'scale': 10.}} for name in ['a', 'b', 'c']}
     #params = {name: {'prior': {'limits': [-1., 1.]}} for name in ['a', 'b']}
     precision = ParameterPrecision(np.eye(len(params)), params=params)
-    covariance = (precision + precision).to_covariance()
+    precision2 = precision + precision
+    assert np.allclose(precision2._value, 2. * precision._value)
+    covariance = precision2.to_covariance()
     print(covariance.to_stats())
     gd = covariance.to_getdist(center=[0.] * len(params))
 
@@ -49,7 +51,7 @@ def test_matrix():
     plotting.plot_triangle(covariance, show=True)
 
     from desilike.samples import plotting
-    plotting.plot_triangle([covariance, covariance.select(name=['a', 'b'])], params=covariance.params(), show=True)
+    plotting.plot_triangle([covariance, covariance.select(name=['a', 'b'])], show=True)
 
     assert np.allclose(covariance.rescale().center(), 1.)
     assert np.ndim(covariance.std('a')) == 0
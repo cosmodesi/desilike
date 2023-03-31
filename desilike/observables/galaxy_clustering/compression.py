c5a7a86c762aeb174cb1808ecc0f98ec36751a8f
Arnaud De-Mattia <adematti@dphppcz231.extra.cea.fr>
2023-03-31 13:33:39 +0200

    do not specify cosmo in CompressionObservable

--- desilike/observables/galaxy_clustering/compression.py
+++ desilike/observables/galaxy_clustering/compression.py
@@ -19,9 +19,9 @@ class BaseCompressionObservable(BaseCalculator):
     conflict_names = []
     meta_names = []
 
-    def initialize(self, extractor=None, data=None, covariance=None, cosmo=None, quantities=None, **kwargs):
+    def initialize(self, extractor=None, data=None, covariance=None, quantities=None, **kwargs):
         self.extractor = extractor
-        self.extractor.init.update(cosmo=cosmo, **kwargs)
+        self.extractor.init.update(**kwargs)
         if quantities is None:
             quantities = get_quantities(self.conflict_names)
         quantities = list(quantities)

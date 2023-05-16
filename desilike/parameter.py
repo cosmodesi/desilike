62739f9f8a74db7ccc74d4ba207ba4ece0f4436f
adematti <arnaud.de-mattia@cea.fr>
2023-05-16 13:28:34 -0700

    indices

--- desilike/parameter.py
+++ desilike/parameter.py
@@ -2125,7 +2125,7 @@ class Samples(BaseParameterCollection):
             return self.get(name)
         new = self.copy()
         try:
-            index = [name] if np.ndim(name) == 0 else name
+            index = [name] if not isinstance(name, slice) and np.ndim(name) == 0 else name
             new.data = [column[index] for column in self.data]
         except IndexError as exc:
             raise IndexError('Unrecognized indices {}'.format(name)) from exc

41eb795e013c64ae0c7aba328407648a9565ec6a
adematti <arnaud.de-mattia@cea.fr>
2023-10-29 01:21:30 -0700

    remove print

--- desilike/base.py
+++ desilike/base.py
@@ -341,7 +341,7 @@ class BasePipeline(BaseClass):
                 if cosmo_params:
                     for basename, name in calculator.runtime_info.base_names.items():
                         if basename in cosmo_params:
-                            print(name, cosmo[conversions.get(basename, basename)])
+                            #print(name, cosmo[conversions.get(basename, basename)])
                             self.input_values[name] = calculator.runtime_info.input_values[basename] = cosmo[conversions.get(basename, basename)]
                 #if set(cosmo_requires.keys()) != {'params'}:  # requires a :class:`cosmoprimo.Cosmology` instance as ``cosmo`` attribute
                 calculator.cosmo = cosmo

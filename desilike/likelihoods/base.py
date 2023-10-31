d75fc9d4842339a43020a5f3e185c5921cb72413
Arnaud De-Mattia <adematti@dphppcz231.extra.cea.fr>
2023-10-31 16:57:58 +0100

    remove print

--- desilike/likelihoods/base.py
+++ desilike/likelihoods/base.py
@@ -88,7 +88,7 @@ class BaseLikelihood(BaseCalculator):
             #pipeline.more_calculate = lambda: None
             self.fisher = getattr(self, 'fisher', None)
             if self.fisher is None or self.fisher.mpicomm is not self.mpicomm or self.fisher.varied_params != solved_params:
-                if self.fisher is not None: print(self.fisher.mpicomm is not self.mpicomm, self.fisher.varied_params != solved_params)
+                #if self.fisher is not None: print(self.fisher.mpicomm is not self.mpicomm, self.fisher.varied_params != solved_params)
                 solve_likelihood = SumLikelihood(solve_likelihoods)
                 all_params = solve_likelihood.all_params
                 #solved_params = ParameterCollection(solved_params)

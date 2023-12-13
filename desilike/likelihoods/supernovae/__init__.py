6d0c09a6dcd8da22f7469953e74df10e7a8c3cda
Rodrigo Calderon <rodrigocalderon10@hotmail.com>
2023-12-13 10:21:54 +0900

    Add Union3 likelihood & NB example

--- desilike/likelihoods/supernovae/__init__.py
+++ desilike/likelihoods/supernovae/__init__.py
@@ -1,3 +1,4 @@
 from .pantheon import PantheonSNLikelihood
 from .pantheonplus import PantheonPlusSNLikelihood
 from .pantheonplusshoes import PantheonPlusSHOESSNLikelihood
+from .union3 import Union3SNLikelihood
--- desilike/likelihoods/tests/test_supernovae.py
+++ desilike/likelihoods/tests/test_supernovae.py
@@ -3,8 +3,7 @@ import numpy as np
 from desilike import setup_logging
 from desilike.install import Installer
 from desilike.theories import Cosmoprimo
-from desilike.likelihoods.supernovae import PantheonSNLikelihood, PantheonPlusSNLikelihood, PantheonPlusSHOESSNLikelihood
-
+from desilike.likelihoods.supernovae import PantheonSNLikelihood, PantheonPlusSNLikelihood, PantheonPlusSHOESSNLikelihood,Union3SNLikelihood
 
 def test_install():
     ref = [-5649.588564438464, -7526.797408441289, -7600.468586494155]
@@ -18,8 +17,14 @@ def test_install():
         assert np.allclose(likelihood.loglikelihood, ref[ilike])
         assert np.allclose((likelihood + likelihood)(Mb=Mb), 2. * likelihood(Mb=Mb) - likelihood.logprior)
 
+def test_install_Union3():
+    cosmo = Cosmoprimo(engine='camb')
+    installer = Installer(user=True)
+    likelihood=Union3SNLikelihood(cosmo=cosmo)
+    installer(likelihood)
 
 if __name__ == '__main__':
 
     setup_logging()
     test_install()
+    test_install_Union3()

3cdbebe8b839a0c497cbf3687b2ff79dd87068eb
adematti <arnaud.de-mattia@cea.fr>
2024-02-12 11:45:58 -0800

    fix velocileptors k; new pocomc

--- desilike/samplers/pocomc.py
+++ desilike/samplers/pocomc.py
@@ -7,6 +7,38 @@ from desilike import utils
 from .base import BaseBatchPosteriorSampler
 
 
+class Prior(object):
+
+    """Prior distribution for PocoMC."""
+
+    def __init__(self, params, random_state=None):
+        self.dists = [param.prior for param in params]
+        self.random_state = random_state
+
+    def logpdf(self, x):
+        logp = np.zeros(len(x))
+        for i, dist in enumerate(self.dists):
+            logp += dist(x[:,i])
+        return logp
+
+    def rvs(self, size=1):
+        samples = []
+        for dist in self.dists:
+            samples.append(dist.sample(size=size, random_state=self.random_state))
+        return np.transpose(samples)
+
+    @property
+    def bounds(self):
+        bounds = []
+        for dist in self.dists:
+            bounds.append(dist.limits)
+        return np.array(bounds)
+
+    @property
+    def dim(self):
+        return len(self.dists)
+
+
 class PocoMCSampler(BaseBatchPosteriorSampler):
     """
     Wrapper for PocoMC sampler (preconditioned Monte Carlo method).
@@ -19,7 +51,8 @@ class PocoMCSampler(BaseBatchPosteriorSampler):
     """
     name = 'pocomc'
 
-    def __init__(self, *args, nwalkers=None, threshold=1.0, scale=True, rescale=False, diagonal=True, flow_config=None, train_config=None, **kwargs):
+    def __init__(self, *args, n_active=None, n_ess=1000, flow=None, train_config=None,
+                 precondition=True, n_prior=None, sample=None, max_steps=None, patience=None, ess_threshold=None, **kwargs):
         """
         Initialize PocoMC sampler.
 
@@ -28,27 +61,46 @@ class PocoMCSampler(BaseBatchPosteriorSampler):
         likelihood : BaseLikelihood
             Input likelihood.
 
-        nwalkers : int, str, default=None
-            Number of walkers, defaults to :attr:`Chain.shape[1]` of input chains, if any,
+        n_active : int, str, default=250
+            The number of active particles (default is ``n_active=250``). It must be smaller than ``n_ess``.
+            Defaults to :attr:`Chain.shape[1]` of input chains, if any,
             else ``2 * max((int(2.5 * ndim) + 1) // 2, 2)``.
             Can be given in dimension units, e.g. '3 * ndim'.
 
-        threshold : float, default=1.0
-            The threshold value for the (normalised) proposal scale parameter below which normalising flow preconditioning (NFP) is enabled.
-            Default is 1.0, meaning that NFP is used all the time.
+        n_ess : int, default=1000
+            The effective sample size maintained during the run (default is ``n_ess=1000``).
 
-        scale : bool, default=True
-            Whether to scale the distribution of particles to have zero mean and unit variance.
-
-        rescale : str, bool, default=False
-            Whether to rescale the distribution of particles to have zero mean and unit variance in every iteration.
-            Pass 'diagonal' to use a diagonal covariance matrix when rescaling instead of a full covariance.
-
-        flow_config : dict, default=None
-            Configuration of the normalizing flow.
+        flow : ``torch.nn.Module`` or ``None``
+            Normalizing flow (default is ``None``). The default is a Masked Autoregressive Flow
+            (MAF) with 6 blocks of 3x64 layers and residual connections.
 
         train_config : dict, default=None
-            Configuration for training the normalizing flow.
+            Configuration for training the normalizing flow
+            (default is ``train_config=None``). Options include a dictionary with the following
+            keys: ``"validation_split"``, ``"epochs"``, ``"batch_size"``, ``"patience"``,
+            ``"learning_rate"``, ``"annealing"``, ``"gaussian_scale"``, ``"laplace_scale"``,
+            ``"noise"``, ``"shuffle"``, ``"clip_grad_norm"``, ``"verbose"``.
+
+        precondition : bool, default=True
+            If True, use preconditioned MCMC (default is ``precondition=True``). If False,
+            use standard MCMC without normalizing flow.
+
+        n_prior : int, default=None
+            Number of prior samples to draw (default is ``n_prior=2*(n_ess//n_active)*n_active``).
+
+        sample : str, default='pcn'
+            Type of MCMC sampler to use (default is ``sample='pcn'``). Options are
+            ``'pcn'`` (Preconditioned Crank-Nicolson) or ``'rwm'`` (Random-Walk Metropolis).
+
+        max_steps : int, default=None
+            Maximum number of MCMC steps (default is ``max_steps=5*n_dim``).
+
+        patience : int, default=None
+            Number of steps for early stopping of MCMC (default is ``patience=None``). If ``patience=None``,
+            MCMC terminates automatically.
+
+        ess_threshold : int, default=None
+            Effective sample size threshold for resampling (default is ``ess_threshold=4*n_dim``).
 
         rng : np.random.RandomState, default=None
             Random state. If ``None``, ``seed`` is used to set random state.
@@ -73,35 +125,26 @@ class PocoMCSampler(BaseBatchPosteriorSampler):
         """
         super(PocoMCSampler, self).__init__(*args, **kwargs)
         ndim = len(self.varied_params)
-        if nwalkers is None:
+        if n_active is None:
             shapes = self.mpicomm.bcast([chain.shape if chain is not None else None for chain in self.chains], root=0)
             if any(shape is not None for shape in shapes):
                 try:
                     nwalkers = shapes[0][1]
                     assert all(shape[1] == nwalkers for shape in shapes)
                 except (IndexError, AssertionError) as exc:
-                    raise ValueError('Impossible to find number of walkers from input chains of shapes {}'.format(shapes)) from exc
-            else:
-                nwalkers = 2 * max((int(2.5 * ndim) + 1) // 2, 2)
-        self.nwalkers = utils.evaluate(nwalkers, type=int, locals={'ndim': ndim})
+                    nwalkers = 250  # default
+            n_active = nwalkers
+        self.nwalkers = utils.evaluate(n_active, type=int, locals={'ndim': ndim})
         bounds = np.array([tuple(None if np.isinf(lim) else lim for lim in param.prior.limits) for param in self.varied_params], dtype='f8')
         import pocomc
-        diagonal = rescale == 'diagonal'
-        rescale = bool(rescale)
-
-        self.sampler = pocomc.Sampler(self.nwalkers, ndim, self.loglikelihood, self.logprior, bounds=bounds, threshold=threshold, scale=scale,
-                                      rescale=rescale, diagonal=diagonal, flow_config=flow_config, train_config=train_config,
-                                      vectorize_likelihood=True, vectorize_prior=True, infer_vectorization=False,
-                                      output_dir=None, output_label=None, random_state=self.rng.randint(0, high=0xffffffff))
+        self.prior = Prior(self.varied_params)
+        self.sampler = pocomc.Sampler(self.prior, self.loglikelihood, ndim, n_ess=n_ess, n_active=self.nwalkers, flow=flow, train_config=train_config, precondition=precondition, n_prior=n_prior, sample=sample, max_steps=max_steps, patience=patience, ess_threshold=ess_threshold, vectorize=True, output_dir=None, output_label=None, random_state=self.rng.randint(0, high=0xffffffff))
         if self.save_fn is None:
             raise ValueError('save_fn must be provided, in order to save pocomc state')
         self.state_fn = [os.path.splitext(fn)[0] + '.pocomc.state' for fn in self.save_fn]
 
     def loglikelihood(self, values):
-        return self.logposterior(values) - self.logprior(values)
-
-    def logprior(self, params, bounds=None):
-        return super(PocoMCSampler, self).logprior(params)
+        return self.logposterior(values) - self.prior.logpdf(values)
 
     def _prepare(self):
         self.resume = self.mpicomm.bcast(any(chain is not None for chain in self.chains), root=0)
@@ -138,42 +181,73 @@ class PocoMCSampler(BaseBatchPosteriorSampler):
         return super(PocoMCSampler, self).run(*args, **kwargs)
 
     def _run_one(self, start, niterations=300, progress=False, **kwargs):
+        load_state_bak = type(self.sampler).load_state
+        resume_state_path = None
+
         if self.resume:
-            self.sampler.load_state(self.state_fn[self._ichain])
-            if self.mpicomm.rank == 0:
-                self.derived = self.sampler.derived
-                del self.sampler.derived
-            from pocomc.tools import FunctionWrapper
-            # Because dill is unable to cope with our loglikelihood and logprior
-            self.sampler.log_likelihood = FunctionWrapper(self.loglikelihood, args=None, kwargs=None)
-            self.sampler.log_prior = FunctionWrapper(self.logprior, args=None, kwargs=None)
-            self.sampler.log_likelihood(self.sampler.x)  # to set derived parameters
+            resume_state_path = self.state_fn[self._ichain]
+
+            def load_state(path):
+                load_state_bak(self.sampler, path)
+                #if self.mpicomm.rank == 0:
+                #    self.derived = self.sampler.derived
+                #    del self.sampler.derived
+                from pocomc.tools import FunctionWrapper
+                # Because dill is unable to cope with our loglikelihood and logprior
+                self.sampler.log_likelihood = FunctionWrapper(self.loglikelihood, args=None, kwargs=None)
+                self.sampler.log_prior = FunctionWrapper(self.logprior, args=None, kwargs=None)
+                x = np.asarray(self.sampler.results.get('x'))
+                self.sampler.log_likelihood(x.reshape(-1, x.shape[-1]))  # to set derived parameters
+                #particles = self.sampler.particles
+                #particles.__init__(n_particles=particles.n_particles, n_dim=particles.n_dim, ess_threshold=particles.ess_threshold)  # clear particles
+        else:
+            def load_state(path):
+                load_state_bak(self.sampler, path)
+
+        self.sampler.load_state = load_state
 
         import torch
         np_random_state_bak, torch_random_state_bak = np.random.get_state(), torch.get_rng_state()
         self.sampler.random_state = self.rng.randint(0, high=0xffffffff)
+        self.prior.random_state = self.rng
         np.random.set_state(self.rng.get_state())  # self.rng is same for all ranks
         torch.set_rng_state(self.mpicomm.bcast(torch_random_state_bak, root=0))
-        if not self.resume:
-            self.sampler.run(prior_samples=start, progress=progress, **kwargs)
 
-        #self.sampler.add_samples(n=niterations)
+        t0 = self.sampler.t
+        nparticles = len(self.sampler.particles.get('x'))
+
+        def _not_termination(current_particles):
+            return (self.sampler.t - t0) < niterations
+
+        self.sampler._not_termination = _not_termination
+        if niterations > 0:
+            self.sampler.run(n_total=niterations, progress=progress, resume_state_path=resume_state_path, **kwargs)  # progress=True else bug
+        else:
+            return None
         np.random.set_state(np_random_state_bak)
         torch.set_rng_state(torch_random_state_bak)
+        #self.sampler.load_state = load_state_bak
+        particles = self.sampler.particles
+        particles.results_dict = None  # to recompute results
         try:
             result = self.sampler.results
         except ValueError:
             return None
         # This is not picklable
-        del self.sampler.log_likelihood, self.sampler.log_prior
+        particles.__init__(n_particles=particles.n_particles, n_dim=particles.n_dim, ess_threshold=particles.ess_threshold)  # clear particles
+        particles.update(self.sampler.current_particles)  # for next iteration, only last particles are needed
+        try:
+            del self.sampler.log_likelihood, self.sampler.log_prior, self.sampler._not_termination, self.sampler.load_state
+        except AttributeError:
+            pass
         # Clear saved quantities to save space
         for name in self.sampler.__dict__:
             if name.startswith('saved_'): setattr(self.sampler, name, [])
         # Save last parameters, which be reused in the next run
         if self.mpicomm.rank == 0:
-            self.sampler.derived = [d[-1:] for d in self.derived]
+            #self.sampler.derived = [d[-1:] for d in self.derived]
             self.sampler.save_state(self.state_fn[self._ichain])  # save ~ all self.sampler.__dict__
-        data = [np.asarray(result['samples'][..., iparam], dtype='f8') for iparam, param in enumerate(self.varied_params)] + [np.asarray(result['loglikelihood'] + result['logprior'], dtype='f8')]
+        data = [np.asarray(result['x'][..., iparam], dtype='f8')[nparticles:] for iparam, param in enumerate(self.varied_params)] + [np.asarray(result['logl'] + result['logp'], dtype='f8')[nparticles:]]
         return Chain(data=data, params=self.varied_params + ['logposterior']).reshape(-1, self.nwalkers)
 
     @classmethod

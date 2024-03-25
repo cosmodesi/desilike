8160af5d0034ca69c1c786e3b7ec4e96b968a66d
arnaud de mattia <adematti@dphppcz231.extra.cea.fr>
2024-03-25 18:21:34 +0100

    removing cobaya sampler

--- desilike/samplers/__init__.py
+++ desilike/samplers/__init__.py
@@ -3,7 +3,6 @@ from .qmc import QMCSampler
 from .emcee import EmceeSampler
 from .zeus import ZeusSampler
 from .pocomc import PocoMCSampler
-from .mcmc import MCMCSampler
 from .dynesty import StaticDynestySampler, DynamicDynestySampler
 from .polychord import PolychordSampler
 from .nautilus import NautilusSampler

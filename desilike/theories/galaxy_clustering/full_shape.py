3b29ccaeee2d7be71d79e4148081c533816dfa48
Diego González <104163582+BlackPuma075@users.noreply.github.com>
2024-09-19 11:46:10 -0600

    Update full_shape.py

--- desilike/theories/galaxy_clustering/full_shape.py
+++ desilike/theories/galaxy_clustering/full_shape.py
@@ -1818,7 +1818,7 @@ class FOLPSPowerSpectrumMultipoles(BasePTPowerSpectrumMultipoles, BaseTheoryPowe
         # [z, omega_b, omega_cdm, omega_ncdm, h]
         # only used for neutrinos
         # sensitive to omega_b + omega_cdm, not omega_b, omega_cdm separately
-        cosmo_params = [self.z, 0.022, 0.12, 0., 0.7, self.fR0]
+        cosmo_params = [self.z, 0.022, 0.12, 0., 0.7, self.template.fR0]
         cosmo = getattr(self.template, 'cosmo', None)
         if cosmo is not None:
             cosmo_params = [self.z, cosmo['omega_b'], cosmo['omega_cdm'], cosmo['omega_ncdm_tot'], cosmo['h'], 1e-20]

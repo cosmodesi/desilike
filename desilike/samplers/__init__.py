52f0c97b5ac3c9ca2cb8d37c264db300f6705908
Johannes U. Lange <julange.astro@pm.me>
2025-07-03 13:01:23 -0400

    more code refactoring, completely new dynesty implementation

--- desilike/samplers/__init__.py
+++ desilike/samplers/__init__.py
@@ -3,12 +3,14 @@ from .qmc import QMCSampler
 from .emcee import EmceeSampler
 from .zeus import ZeusSampler
 from .pocomc import PocoMCSampler
-from .dynesty import StaticDynestySampler, DynamicDynestySampler
+from .dynesty import DynestySampler
 from .polychord import PolychordSampler
 from .nautilus import NautilusSampler
 from .hmc import HMCSampler
 from .nuts import NUTSSampler
 from .mclmc import MCLMCSampler
 from .importance import ImportanceSampler
-try: from .mcmc import MCMCSampler
-except ImportError: pass
+try:
+    from .mcmc import MCMCSampler
+except ImportError:
+    pass

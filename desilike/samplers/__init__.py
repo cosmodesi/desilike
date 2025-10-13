f79c447e2b4e125e5c90e30e5bfb468aae2e7042
Johannes U. Lange <julange.astro@pm.me>
2025-10-13 16:58:24 -0400

    re-implemented NUTS

--- desilike/samplers/__init__.py
+++ desilike/samplers/__init__.py
@@ -1,18 +1,17 @@
 """Module providing wrappers for commonly used samplers."""
 
+from .blackjax import HMCSampler, NUTSSampler
 from .dynesty import DynestySampler
 from .emcee import EmceeSampler
 from .grid import GridSampler
-from .hmc import HMCSampler
 from .importance import ImportanceSampler
 # from .mclmc import MCLMCSampler
 # from .mcmc import MCMCSampler
 from .nautilus import NautilusSampler
-# from .nuts import NUTSSampler
 from .pocomc import PocoMCSampler
 from .qmc import QMCSampler
 from .zeus import ZeusSampler
 
 __all__ = ['DynestySampler', 'GridSampler', 'HMCSampler', 'EmceeSampler',
-           'ImportanceSampler', 'NautilusSampler', 'PocoMCSampler',
-           'QMCSampler', 'ZeusSampler']
+           'ImportanceSampler', 'NautilusSampler', 'NUTSSampler',
+           'PocoMCSampler', 'QMCSampler', 'ZeusSampler']

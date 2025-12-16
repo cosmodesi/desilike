83688cdc239452aecac969fdfad297af6588fcce
gqcristhian <gqcristhian@gmail.com>
2025-12-16 01:22:27 -0500

    adding slurm script for jiaxi

---
+++ MG/running_scripts/run_desilike_synthetic_DV.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 4                  # 4 MPI ranks
+#SBATCH -c 28                 # 28 threads per rank -> 4*28 = 112 cores total
+#SBATCH --mem=50GB               # take full node memory
+#SBATCH -t 48:00:00
+#SBATCH -p itc_cluster,sapphire
+#SBATCH -J mcmc_LCDM_emu_phys_mu0_beds_max_new
+#SBATCH -o logs/%x_%j.out
+#SBATCH -e logs/%x_%j.err
+
+# ---- prep logs dir ----
+mkdir -p logs
+
+# ---- user env ----
+source ~/.bashrc
+conda activate cosmodesi
+
+# ---- modules (match your previous stack) ----
+module load cmake/3.25.2-fasrc01
+module load gmp/6.2.1-fasrc01
+module load mpfr/4.1.0-fasrc01
+module load mpc/1.2.1-fasrc01
+module load gcc/10.2.0-fasrc01
+module load openmpi/4.1.1-fasrc01
+
+# ---- threading (BLAS/OpenMP) ----
+export OMP_NUM_THREADS="${SLURM_CPUS_PER_TASK}"
+export MKL_NUM_THREADS="${OMP_NUM_THREADS}"
+export OPENBLAS_NUM_THREADS="${OMP_NUM_THREADS}"
+export OMP_PLACES=cores
+export OMP_PROC_BIND=spread
+export PYTHONUNBUFFERED=1
+
+cd /n/home12/cgarciaquintero/DESI/MG_validation/synthetic_noiseless/desilike_runs/
+
+# RUN MCMC
+srun --mpi=pmix -n "${SLURM_NTASKS}" -c "${SLURM_CPUS_PER_TASK}" \
+  python -u run_desilike.py \
+    --use-emu \
+    --ells 0,2 \
+    --freedom max \
+    --fid-model LCDM \
+    --MG-model HDKI \
+    --mg-variant mu_OmDE \
+    --priors-basis physical_velocileptors \
+    --beyond-eds \
+    --resume \
+    --chain-prefix "isitgr_fkpt_noextf_new" \
+    --chains-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/chains \
+    --emu-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/Emulators
+
+#python -u run_desilike.py --use-emu --ells 0,2 --freedom max --fid-model LCDM --MG-model HDKI --mg-variant mu_OmDE --chain-prefix "test" --mode nautilus --chains-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/chains  --emu-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/Emulators
+
+# RUN MINIMIZATION
+
+#srun --mpi=pmix -n "${SLURM_NTASKS}" -c "${SLURM_CPUS_PER_TASK}" \
+#  python -u run_desilike.py \
+#    --use-emu \
+#    --ells 0,2 \
+#    --freedom min \
+#    --model LCDM \
+#    --redshift-bins \
+#    --mode map \
+#    --chain-prefix "chain_fs_direct_folps_isitgr" \
+#    --chains-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/chains  \
+#    --emu-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/Emulators
+
+# BUILD EMULATOR
+
+#srun --mpi=pmix -n "${SLURM_NTASKS}" -c "${SLURM_CPUS_PER_TASK}" \
+#  python -u run_desilike.py \
+#    --create-emu \
+#    --ells 0,2 \
+#    --freedom max \
+#    --model LCDM \
+#    --chains-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/chains  \
+#    --emu-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/Emulators
+
+#srun --mpi=pmix -n "${SLURM_NTASKS}" -c "${SLURM_CPUS_PER_TASK}" \
+#  python -u run_desilike.py \
+#    --create-emu \
+#    --ells 0,2,4 \
+#    --freedom min \
+#    --model LCDM \
+#    --redshift-bins \
+#    --chains-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/chains  \
+#    --emu-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/Emulators
+
+#srun --mpi=pmix -n "${SLURM_NTASKS}" -c "${SLURM_CPUS_PER_TASK}" \
+#  python -u run_desilike.py \
+#    --create-emu \
+#    --ells 0,2,4 \
+#    --freedom min \
+#    --model LCDM \
+#    --redshift-bins --scale-bins --kc 0.01 \
+#    --chains-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/chains  \
+#    --emu-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/Emulators
+
+#srun --mpi=pmix -n "${SLURM_NTASKS}" -c "${SLURM_CPUS_PER_TASK}" \
+#  python -u run_desilike.py \
+#    --create-emu \
+#    --ells 0,2 \
+#    --freedom min \
+#    --model LCDM \
+#    --redshift-bins --scale-bins --kc 0.1 \
+#    --chains-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/chains \
+#    --emu-dir /n/netscratch/eisenstein_lab/Lab/cristhian/desilike/Emulators

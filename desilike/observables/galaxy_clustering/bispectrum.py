5c11009770c8cf68cc7b50bcd857f35f12583495
arnaud de mattia <adematti@dphppcz231.extra.cea.fr>
2026-03-03 22:21:31 +0100

    bispectrum observable

--- desilike/observables/galaxy_clustering/bispectrum.py
+++ desilike/observables/galaxy_clustering/bispectrum.py
@@ -1,16 +1,17 @@
-import glob
-
 import numpy as np
+import scipy as sp
+from matplotlib import pyplot as plt
 import lsstypes as types
 
-from desilike import plotting, jax, utils
+from desilike import plotting
 from desilike.base import BaseCalculator
 from desilike.jax import numpy as jnp
-from desilike.observables.types import ObservableArray, ObservableCovariance
 
 
-def _is_array(data):
-    return isinstance(data, (np.ndarray,) + jax.array_types)
+def _make_list(array, nitems=1):
+    if not isinstance(array, (tuple, list)):
+        array = [array] * nitems
+    return list(array)
 
 
 class TracerBispectrumMultipolesObservable(BaseCalculator):
@@ -19,50 +20,101 @@ class TracerBispectrumMultipolesObservable(BaseCalculator):
 
     Parameters
     ----------
-    data : array, dict, default=None
-        Data power spectrum measurement: flat array (of all multipoles).
-        If dict, parameters to be passed to theory to generate mock measurement.
-        Additionally provide list of multipoles ``ells`` and wavenumbers ``k`` and optionally ``shotnoise``.
-
+    data : array, lsstypes.Mesh3SpectrumPoles, default=None
+        Data bispectrum measurement. Either:
+        - flat array (of all multipoles). Additionally provide list of multipoles ``ells`` and wavenumbers ``k`` and optionally ``shotnoise``;
+        - :class:`lsstypes.Mesh3SpectrumPoles` (contains all necessary information).
+    window : array, lsstypes.WindowMatrix, default=None
+        Window matrix. Either:
+        - ``None``. A trivial window matrix is assumed.
+        - 2D array. Additionally provide input array ``kin`` and input multipoles ``ellsin``.
+        - :class:`lsstypes.WindowMatrix` (contains all necessary information).
     covariance : array, list, default=None
-        2D array, of shape ``(len(data), len(data))``.
-
-    k : tuple of arrays, default=None
-        Triangles of wavenumbers of shape (nk, 3) (scoccimarro basis) where to evaluate multipoles.
-        ``sum(len(kk) for kk in k)`` should match ``len(data)``.
-
-    ells : tuple, default=((0, 0, 0), (2, 0, 0), (0, 2, 0), (0, 0, 2))
-        Bispectrum multipoles to compute.
-
-    shotnoise : array, default=1e4
-        Shot noise for each of the multipoles. Same length as ``k``.
-
+        Covariance matrix. Either:
+        - 2D array, of shape ``(len(data), len(data))``.
+        - :class:`lsstypes.CovarianceMatrix` (contains all necessary information).
     theory: BaseCalculator
         Theory for the bispectrum.
-
+    k : tuple of arrays, default=None
+        If ``data`` is an array. List of wavenumbers (for each multipole) of shape (nk, 3) (Scoccimarro basis)
+        or (nk, 2) (Sugiyama basis). ``sum(len(kk) for kk in k)`` should match ``len(data)``.
+    ells : tuple
+        Data bispectrum multipole orders. Typically:
+        - [(0, 0, 0), (2, 0, 2)] for the Sugiyama basis;
+        - [0, 2] for the Scoccimarro basis.
+    kin : array, optional
+        If ``window`` is a 2D array. Theory wavenumbers (assumed same for each multipole).
+    ellsin : tuple
+        If ``window`` is a 2D array. Theory bispectrum multipole orders. Typically:
+        - [(0, 0, 0), (2, 0, 2)] for the Sugiyama basis;
+        - [0, 2] for the Scoccimarro basis.
+    shotnoise : float, optional
+        Shot noise to scale theory stochastic parameters.
     """
     name = 'spectrum3poles'
 
-    def initialize(self, data=None, covariance=None, k=None, kedges=None, ells=None, shotnoise=None, theory=None):
+    def initialize(self, data=None, window=None, covariance=None, theory=None,
+                   k=None, ells=None, basis='sugiyama',
+                   kin=None, ellsin=None, shotnoise=None):
         assert data is not None
         assert theory is not None
-        self.kedges = kedges
-        self.wmatrix = theory
-        self.wmatrix.init.update(k=k, ells=ells, shotnoise=shotnoise)
-        self.wmatrix.runtime_info.initialize()
-        for name in ['k', 'ells', 'shotnoise']:
-            setattr(self, name, getattr(self.wmatrix, name))
-        self.flatdata = None
-        if not isinstance(data, dict):
-            self.flatdata = np.concatenate(data)
-        if self.flatdata is None:
-            self.wmatrix(**data)
-            self.flatdata = np.concatenate(self.wmatrix.power)
-        self.covariance = None
-        if self.mpicomm.bcast(_is_array(covariance) or isinstance(covariance, ObservableCovariance), root=0):
-            self.covariance = self.mpicomm.bcast(covariance, root=0)
-        elif covariance is not None:
-            raise ValueError('Provide covariance array')
+        self.theory = theory
+        custom_data = not isinstance(data, types.ObservableLike)
+        custom_window = not isinstance(window, types.WindowMatrix)
+        custom_covariance = not isinstance(covariance, types.CovarianceMatrix)
+        if custom_data:
+            data = np.ravel(data)
+            for name, value in {'k': k, 'ells': ells, 'shotnoise': shotnoise}.items():
+                if value is None:
+                    raise ValueError(f'when input data is an array, provide {name}')
+            if basis == 'sugiyama':
+                if np.ndim(ells) == 0:
+                    raise ValueError('input ell should be a list of tuples, e.g. [(0, 0, 0), (2, 0, 2)]')
+                if np.dim(ells[0]) == 0:
+                    ells = [ells]
+                ells = [tuple(ell) for ell in ells]
+            k = _make_list(k, len(ells))
+            ksizes = tuple(len(kk) for kk in k)
+            assert sum(ksizes) == data.size, f'total k-size should be the same as data, but got {ksizes} and {data.size}'
+            value = data
+            data = types.ObservableTree(ells=[])
+            start = 0
+            for ell, kk in zip(ells, k):
+                stop = start + len(kk)
+                leaf = types.ObservableLeaf(k=kk, value=value[start:stop], coords=['k'], meta={'basis': basis})
+                start = stop
+                data.insert(leaf, ells=ell)
+        if window is None:
+            kin = np.unique(np.concatenate([pole.coords('k') for pole in data], axis=0))
+            window = []
+            for label, pole in data.items():
+                tmp = np.zeros((pole.size, kin.size))
+                index = np.searchsorted(kin, pole.coords('k'), side='left')
+                tmp[:, index] = 1.
+                window.append(tmp)
+            window = sp.linalg.block_diag(window)
+        if custom_window:
+            window = np.array(window)
+            assert window.ndim == 2
+            theory = types.ObservableTree(ells=[])
+            start = 0
+            for ell in ellsin:
+                leaf = types.ObservableLeaf(k=kin, value=np.zeros_like(kin), coords=['k'])
+                theory.insert(leaf, ells=ell)
+            window = types.WindowMatrix(value=window, theory=theory, observable=data.clone(value=np.zeros_like(data.value())))
+        elif not custom_data:  # match
+            window = window.at.observable.match(data)
+        assert window.shape[0] == data.size, f'output window dimension should match data size, but got {window.shape[0]} != {data.size}'
+        if covariance is not None:
+            if custom_covariance:
+                covariance = np.array(covariance)
+                assert covariance.ndim == 2
+                covariance = types.CovarianceMatrix(value=covariance, observable=data.clone(value=np.zeros_like(data.value())))
+            elif not custom_data:  # match
+                covariance = covariance.at.observable.match(data)
+            assert covariance.shape[0] == data.size, 'covariance shape should match data size'
+        self.data, self.window, self.covariance = data, window, covariance
+        self.theory.init.update(k=next(iter(self.window.theory)).coords('k'), ells=self.window.theory.ells)
 
     @plotting.plotter(interactive={'kw_theory': {'color': 'black', 'label': 'reference'}})
     def plot(self, kw_theory=None, fig=None):
@@ -73,20 +125,15 @@ class TracerBispectrumMultipolesObservable(BaseCalculator):
         ----------
         kw_theory : list of dict, default=None
             Change the default line parametrization of the theory, one dictionary for each ell or duplicate it.
-
         fig : matplotlib.figure.Figure, default=None
             Optionally, a figure with at least ``1 + len(self.ells)`` axes.
-
         fn : str, Path, default=None
             Optionally, path where to save figure.
             If not provided, figure is not saved.
-
         kw_save : dict, default=None
             Optionally, arguments for :meth:`matplotlib.figure.Figure.savefig`.
-
         show : bool, default=False
             If ``True``, show figure.
-
         interactive : bool, default=False
             If ``True``, use interactive interface provided by ipywidgets.
 
@@ -94,18 +141,17 @@ class TracerBispectrumMultipolesObservable(BaseCalculator):
         -------
         fig : matplotlib.figure.Figure
         """
-        from matplotlib import pyplot as plt
-
         if kw_theory is None:
             kw_theory = {}
         if isinstance(kw_theory, dict):
             kw_theory = [kw_theory]
-        if len(kw_theory) != len(self.ells):
-            kw_theory = [{key: value for key, value in kw_theory[0].items() if (key != 'label') or (ill == 0)} for ill in range(len(self.ells))]
-        kw_theory = [{'color': 'C{:d}'.format(ill), **kw} for ill, kw in enumerate(kw_theory)]
+        labels = self.data.labels()
+        if len(kw_theory) != len(labels):
+            kw_theory = [{key: value for key, value in kw_theory[0].items() if (key != 'label') or (ill == 0)} for ill in range(len(labels))]
+        kw_theory = [{'color': f'C{ill:d}', **kw} for ill, kw in enumerate(kw_theory)]
 
         if fig is None:
-            height_ratios = [max(len(self.ells), 3)] + [1] * len(self.ells)
+            height_ratios = [max(len(labels), 3)] + [1] * len(labels)
             figsize = (6, 1.5 * sum(height_ratios))
             fig, lax = plt.subplots(len(height_ratios), sharex=True, sharey=False, gridspec_kw={'height_ratios': height_ratios}, figsize=figsize, squeeze=True)
             fig.subplots_adjust(hspace=0.1)
@@ -114,175 +160,49 @@ class TracerBispectrumMultipolesObservable(BaseCalculator):
             lax = fig.axes
             show_legend = False
 
-        data, theory, std = self.data, self.theory, self.std
-        ik = [np.arange(len(kk)) for kk in self.k]
-
+        wtheory = self.data.clone(value=self.flattheory)
+        for label in labels:
+            data_pole = self.data.get(**label)
+            wtheory_pole = wtheory.get(**label)
+            if 'scoccimarro' in data_pole.basis:
+                x = np.arange(data_pole.size)
+                scale = data_pole.coords('k').prod(axis=-1)
+                xlabel = r'$k$ triangle index'
+                ylabel = r'$k_1 k_2 k_3 B_{\ell}(k_1, k_2, k_3)$ [$(\mathrm{Mpc}/h)^3$]'
+            elif 'sugiyama' in data_pole.basis:
+                k = data_pole.coords('k')
+                scale = k.prod(axis=-1)
+                if np.allclose(k[..., 1], k[..., 0]):
+                    x = k[..., 0]
+                    xlabel = r'$k$ [h/\mathrm{Mpc}]'
+                    ylabel = r'$k^2 B_{\ell}(k, k)$ [$(\mathrm{Mpc}/h)^4$]'
+                else:
+                    x = np.arange(data_pole.size)
+                    xlabel = r'$k$ triangle index'
+                ylabel = r'$k^2 B_{\ell}(k, k)$ [$(\mathrm{Mpc}/h)^4$]'
+            std = self.covariance.at.observable.get(**label).std()
+            lax[0].errorbar(x, scale * data_pole.value(), yerr=scale * std, color=f'C{ill:d}', linestyle='none', marker='o', label=rf'$\ell = {ell}$')
+            lax[0].plot(x, scale * wtheory_pole.value(), **kw_theory[ill])
         for ill, ell in enumerate(self.ells):
-            lax[0].errorbar(ik[ill], data[ill], yerr=std[ill], color='C{:d}'.format(ill), linestyle='none', marker='o', label=r'$\ell = {}$'.format(ell))
-            lax[0].plot(ik[ill], theory[ill], **kw_theory[ill])
-        for ill, ell in enumerate(self.ells):
-            lax[ill + 1].plot(ik[ill], (data[ill] - theory[ill]) / std[ill], **kw_theory[ill])
+            lax[ill + 1].plot(x, (data_pole.value() - wtheory_pole.value()) / std, **kw_theory[ill])
             lax[ill + 1].set_ylim(-4, 4)
             for offset in [-2., 2.]: lax[ill + 1].axhline(offset, color='k', linestyle='--')
-            lax[ill + 1].set_ylabel(r'$\Delta B_{{{0}}} / \sigma_{{ B_{{{0}}} }}$'.format(ell))
+            lax[ill + 1].set_ylabel(rf'$\Delta B_{{{ell}}} / \sigma_{{ B_{{{ell}}} }}$')
+
         for ax in lax: ax.grid(True)
         if show_legend: lax[0].legend()
-        lax[0].set_ylabel(r'$B_{\ell}(k)$ [$(\mathrm{Mpc}/h)^{6}$]')
-        scaling = 'log'
-        if scaling == 'loglog':
-            lax[0].set_yscale('log')
-        lax[-1].set_xlabel(r'$k$ triangle index')
+        lax[0].set_ylabel(ylabel)
+        lax[-1].set_xlabel(xlabel)
         return fig
-
-    @plotting.plotter
-    def plot_bao(self, fig=None):
-        """
-        Plot data and theory BAO bispectrum wiggles.
-
-        Parameters
-        ----------
-        fig : matplotlib.figure.Figure, default=None
-            Optionally, a figure with at least ``len(self.ells)`` axes.
-
-        fn : str, Path, default=None
-            Optionally, path where to save figure.
-            If not provided, figure is not saved.
-
-        kw_save : dict, default=None
-            Optionally, arguments for :meth:`matplotlib.figure.Figure.savefig`.
-
-        show : bool, default=False
-            If ``True``, show figure.
-
-        Returns
-        -------
-        fig : matplotlib.figure.Figure
-        """
-        from matplotlib import pyplot as plt
-        if fig is None:
-            height_ratios = [1] * len(self.ells)
-            figsize = (6, 2 * sum(height_ratios))
-            fig, lax = plt.subplots(len(height_ratios), sharex=True, sharey=False, gridspec_kw={'height_ratios': height_ratios}, figsize=figsize, squeeze=False)
-            lax = lax.ravel()  # in case only one ell
-            fig.subplots_adjust(hspace=0)
-        else:
-            lax = fig.axes
-        data, theory, std = self.data, self.theory, self.std
-        nobao = self.theory_nobao
-        ik = [np.arange(len(kk)) for kk in self.k]
-
-        for ill, ell in enumerate(self.ells):
-            lax[ill].errorbar(ik[ill], data[ill] - nobao[ill], yerr=std[ill], color='C{:d}'.format(ill), linestyle='none', marker='o')
-            lax[ill].plot(ik[ill], theory[ill] - nobao[ill], color='C{:d}'.format(ill))
-            lax[ill].set_ylabel(r'$k \Delta B_{{{:d}}}(k)$ [$(\mathrm{{Mpc}}/h)^{{6}}$]'.format(ell))
-        for ax in lax: ax.grid(True)
-        lax[-1].set_xlabel(r'$k$ triangle index')
-        return fig
-
-    @plotting.plotter
-    def plot_covariance_matrix(self, corrcoef=True, **kwargs):
-        """
-        Plot covariance matrix.
-
-        Parameters
-        ----------
-        corrcoef : bool, default=True
-            If ``True``, plot the correlation matrix; else the covariance.
-
-        barlabel : str, default=None
-            Optionally, label for the color bar.
-
-        figsize : int, tuple, default=None
-            Optionally, figure size.
-
-        norm : matplotlib.colors.Normalize, default=None
-            Scales the covariance / correlation to the canonical colormap range [0, 1] for mapping to colors.
-            By default, the covariance / correlation range is mapped to the color bar range using linear scaling.
-
-        labelsize : int, default=None
-            Optionally, size for labels.
-
-        fig : matplotlib.figure.Figure, default=None
-            Optionally, a figure with at least ``len(self.ells) * len(self.ells)`` axes.
-
-        Returns
-        -------
-        fig : matplotlib.figure.Figure
-        """
-        covariance = self.to_lsstypes('covariance')
-        return covariance.plot(corrcoef=corrcoef, **kwargs)
 
     def calculate(self):
         # Set flattheory with bispectrum prediction
-        self.flattheory = jnp.concatenate(self.wmatrix.power)
-
-    @property
-    def theory(self):
-        return self.wmatrix.power
-
-    @property
-    def theory_nobao(self):
-        # Remove BAO wiggles, a bit hacky
-        template = self.wmatrix.template
-        only_now = template.only_now
-        template.only_now = True
-
-        def callback(calculator):
-            all_requires.append(calculator)
-            for require in calculator.runtime_info.requires:
-                if require in all_requires:
-                    del all_requires[all_requires.index(require)]  # we want first dependencies at the end
-                callback(require)
-
-        all_requires = []
-        callback(self)
-        all_requires = all_requires[::-1]
-
-        for calculator in all_requires:
-            calculator.runtime_info.tocalculate = True
-            calculator.runtime_info.calculate(calculator.runtime_info.input_values)
-
-        nobao = self.theory
-
-        template.only_now = only_now
-        for calculator in all_requires:
-            calculator.runtime_info.tocalculate = True
-            calculator.runtime_info.calculate(calculator.runtime_info.input_values)
-
-        return nobao
-
-    @property
-    def data(self):
-        # data, split by multipoles
-        cumsize = np.insert(np.cumsum([len(k) for k in self.k]), 0, 0)
-        return [self.flatdata[start:stop] for start, stop in zip(cumsize[:-1], cumsize[1:])]
-
-    @property
-    def std(self):
-        # Standard deviation, split by multipoles
-        cumsize = np.insert(np.cumsum([len(k) for k in self.k]), 0, 0)
-        diag = np.diag(self.covariance)**0.5
-        return [diag[start:stop] for start, stop in zip(cumsize[:-1], cumsize[1:])]
+        self.flattheory = jnp.dot(self.window.value(), self.theory.power)
 
     def __getstate__(self):
         # Optional
         state = {}
-        for name in ['k', 'ells', 'flatdata', 'flattheory', 'shotnoise']:
+        for name in ['flatdata', 'flattheory']:
             if hasattr(self, name):
                 state[name] = getattr(self, name)
         return state
-
-    def to_lsstypes(self, kind):
-        """Return data or covariance."""
-        data = [types.Mesh3PowerSpectrumPole(k=self.k[ill], k_edges=self.kedges[ill], value=self.data[ill], ell=ell) for ell in self.ells]
-        data = types.Mesh2PowerSpectrumPoles(data)
-        if kind == 'data':
-            return data
-        if kind == 'covariance':
-             return types.CovarianceMatrix(observable=data, value=self.covariance)
-        raise NotImplementedError(f'kind {kind} not recognized')
-
-    def to_array(self):
-        warnings.warn('to_array is deprecated. Please use to_lsstypes')
-        from desilike.observables import ObservableArray
-        return ObservableArray(x=self.k, value=self.data, projs=self.ells, attrs={'shotnoise': self.shotnoise}, name=self.__class__.__name__)
